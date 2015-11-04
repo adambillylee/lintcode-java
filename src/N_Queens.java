be2295886e9d9b597a2f86a273bf0d9852a5aa68
Adam Li <adam.li@ensighten.com>
2015-11-03 23:51:10 -0800

    submit: Nov 3nd, final version of n queens

--- src/N_Queens.java
+++ src/N_Queens.java
@@ -14,42 +14,79 @@ public class N_Queens {
      * For example, A string '...Q' shows a queen on forth position
      */
     ArrayList<ArrayList<String>> solveNQueens(int n) {
-        ArrayList<ArrayList<String>> rst = new ArrayList<ArrayList<String>>();
-
-        // write your code here
         this.n = n;
+
         ArrayList<Integer> cols = new ArrayList<Integer>();
 
         helper(cols, 0);
 
-        return this.rst;
+        return rst;
     }
 
     public void helper(ArrayList<Integer> cols, int row) {
-        // when last recur call is at last row, it will make row = n+1
+        // if config for all rows are filled, add new config into rst
         if (cols.size() == n) {
             rst.add(drawBoard(cols));
             return;
         }
 
-        for (int j = 0; j<n; j++) {
-            if (isValid(cols, row, j)) {
+        for (int j=0; j<n; j++) {
+            if (isValid(cols, row, j)){
+                // add new valid col index into new row in cols
                 cols.add(j);
-                helper(cols, row + 1);
-                cols.remove(cols.size() - 1);
+
+                // REMEMBER TO USE row+1 INSTEAD OF row++
+                helper(cols, row+1);
+
+                // remove last col value for backtracking
+                cols.remove(cols.size()-1);
             }
         }
     }
 
-    public boolean isValid(ArrayList<Integer> cols, int row, int col) {
+    public ArrayList<String> drawBoard(ArrayList<Integer> cols) {
+        ArrayList<String> config = new ArrayList<String>();
+
+        for(int i=0; i<n; i++) {
+            // create a new char array for all chars in curr line
+            char line[] = new char[n];
+            for(int j=0; j<n; j++) {
+                // put 'Q' or '.' into ling
+                if (cols.get(i) == j) {
+                    line[j] = 'Q';  // use single quote here
+                }else {
+                    line[j] = '.';  // use single quote here
+                }
+            }
+
+            // convert char array into string and add it into config
+            String tmp = new String(line);
+            config.add(tmp);
+        }
+
+        return config;
+    }
+
+    /**
+     * decide if a queen position is valid given previous queens configuration
+     * @param cols previous queens configuration, cols.get(i) = col index of queen on row i
+     * @param row new queen row index
+     * @param col new queen column index
+     * @return valid or not
+     */
+    public boolean isValid(ArrayList<Integer>cols, int row, int col){
         // check "same column"
         for (int i=0; i<row; i++) {
             if (cols.get(i) == col)
                 return false;
         }
 
-        // check "same diagnal"
+        // check diagonal
         for (int i=0; i<row; i++) {
+            /**
+             * if absolute diff of col and row are the same for two queens
+             * they are on same diagonal
+             */
             if (row - i == Math.abs(col - cols.get(i)))
                 return false;
         }
@@ -57,29 +94,8 @@ public class N_Queens {
         return true;
     }
 
-    public ArrayList<String> drawBoard(ArrayList<Integer> cols) {
-        ArrayList<String> board = new ArrayList<String>();
-
-        for (int i=0; i<n; i++) {
-            char line[] = new char[n];
-
-            for (int j=0; j<n; j++) {
-                if (cols.get(i) == j) {
-                    line[j] = 'Q';
-                }else{
-                    line[j] = '.';
-                }
-            }
-
-            String tmp = new String(line);
-            board.add(tmp);
-        }
-
-        return board;
-    }
-
     public static void main (String[] args) {
-        int n = 1;
+        int n = 2;
         N_Queens sol = new N_Queens();
         ArrayList<ArrayList<String>> rst = sol.solveNQueens(n);
         System.out.println();
