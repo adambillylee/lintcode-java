ae89988b7a6983b7ef1ec86834d098004a1e66c2
Adam Li <adam.li@ensighten.com>
2015-11-04 23:00:15 -0800

    submit: Nov 4st, final version of word ladder

--- src/Word_Ladder.java
+++ src/Word_Ladder.java
@@ -12,43 +12,46 @@ public class Word_Ladder {
      * @return an integer
      */
     public int ladderLength(String start, String end, Set<String> dict) {
+        // from start to any node has a initial path length of 1
         int pathLength = 1;
 
-        // use a hash set to record all traversed nodes in graph
-        HashSet<String> hash = new HashSet<String>();
+        // create a hashset to record visited strings for de-duplication
+        HashSet<String> visited = new HashSet<String>();
 
-        // make a queue for BST
+        // create a queue to record all nodes in current level of "breath"
         LinkedList<String> queue = new LinkedList<String>();
 
+        // initiation: add start into hash and queue
+        visited.add(start);
         queue.offer(start);
-        hash.add(start);
 
         /**
-         * BST
-         * 1st loop: check if queue is empty, if empty, end BST
+         * 3 loops for BST
+         * 1st loop: keep BST running until queue is empty
          */
         while (!queue.isEmpty()) {
-            // get the size of current queue
             int size = queue.size();
 
-            // add path length +1 for one more breath level
+            // if queue is not empty, we enter a new lvl of breath, add path length by 1
             pathLength++;
-            // 2nd loop: loop through the queue (all words in current breath lvl)
-            for (int i=0; i<size; i++) {
+
+            /**
+             * 2dn loop: loop through all nodes in current queue
+             */
+            for(int i=0; i<size; i++) {
                 String word = queue.poll();
 
-                // 3rd loop: loop through all possible next words for curr word
-                // (one char diff than curr word and in dict)
-                for (String nextWord: getNextWord(word, dict)) {
-                    // the word has been checked before, don't duplicate the path
-                    if (hash.contains(nextWord))
-                        continue;
-
-                    if(nextWord.equals(end)) {
+                /**
+                 * 3rd loop: loop through all possible next word for current word
+                 */
+                for (String nextWord: getNextWords(word, dict)) {
+                    // it next word is end, return path length
+                    if (nextWord.equals(end)) {
                         return pathLength;
                     }else{
-                        queue.offer(nextWord);
-                        hash.add(nextWord);
+                        // if not, add next word into queue (next lvl of breath) and visited set
+                        visited.add(nextWord);
+                        queue.add(nextWord);
                     }
                 }
             }
@@ -57,33 +60,27 @@ public class Word_Ladder {
         return pathLength;
     }
 
-    /**
-     * get all possible next word based on curr word an dict
-     * @param word
-     * @return
-     */
-    public ArrayList<String> getNextWord(String word, Set<String> dict) {
+    private ArrayList<String> getNextWords(String word, Set<String> dict) {
         ArrayList<String> rst = new ArrayList<String>();
 
+        // list all possibilities of next alphabet
         String alphabets = "abcdefghijklmnopqrstuvwxyz";
 
         int len = word.length();
+        for(int i=0; i<len; i++) {
+            for (int j=0; j< alphabets.length(); j++) {
+                char letters[] = word.toCharArray();
+                letters[i] = alphabets.charAt(j);
+                String replaced = new String(letters);
 
-        for(int i=0; i<len-1; i++) {
-            for(int j=0; j<alphabets.length();j++) {
-                char[] wordChar = word.toCharArray();
-                wordChar[i] = alphabets.charAt(j);
-                String newWord = new String(wordChar);
-
-                if (dict.contains(newWord))
-                    rst.add(newWord);
+                // if new word is in dict, add it into result
+                if (dict.contains(replaced)) {
+                    rst.add(replaced);
+                }
             }
         }
 
         return rst;
     }
 
-    public static void main(String args[]) {
-        Word_Ladder sol = new Word_Ladder();
-    }
 }
