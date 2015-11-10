12614985dd657deeb78570208a79c1be4c069624
Adam Li <adam.li@ensighten.com>
2015-11-10 01:43:23 -0800

    submit: Nov 9th, PriorityQueue version of Top K Most Frequent Words

--- src/Top_K_Frequent_Words.java
+++ src/Top_K_Frequent_Words.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xalan.internal.xsltc.dom.MultiValuedNodeHeapIterator;
+
 import java.util.*;
 
 /**
@@ -5,42 +7,109 @@ import java.util.*;
  */
 public class Top_K_Frequent_Words {
     /**
+     * Solve it by using HashMap
      * @param words an array of string
      * @param k     an integer
      * @return an array of string
      */
+//    public String[] topKFrequentWords(String[] words, int k) {
+//        // define a hash map to record frequency
+//        HashMap<String, Integer> map = new HashMap<String, Integer>();
+//
+//        int len = words.length;
+//
+//        // compute all word frequency and put into hashmap
+//        for (String word : words) {
+//            // if word freq in map, add it by 1
+//            if (map.containsKey(word)) {
+//                map.put(word, map.get(word)+1);
+//            }else{
+//                map.put(word, 1);
+//            }
+//        }
+//
+//        // create an ArrayList for purpose of sorting map elements
+//        ArrayList<Map.Entry<String ,Integer>> tmp = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+//
+//        // sort the elements in map from highest frequency to lowest
+//        Collections.sort(tmp, new Comparator<Map.Entry<String, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+//                if (o1.getValue() == o2.getValue()) {
+//                    /**
+//                     * if word freq of both map ele is same
+//                     * return the one with lower alphabet order
+//                     */
+//                    return o1.getKey().compareTo(o2.getKey());
+//                }else{
+//                    return o2.getValue() - o1.getValue();
+//                }
+//            }
+//        });
+//
+//        // put top k elements into result set
+//        String rst[] = new String[k];
+//
+//        for (int i=0; i<k; i++) {
+//            rst[i] = tmp.get(i).getKey();
+//        }
+//
+//        return rst;
+//    }
+
     public String[] topKFrequentWords(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-
-        int len = words.length;
-
-        for(String word: words) {
-            if (map.containsKey(word)){
-                map.put(word, map.get(word) + 1);
-            }else{
-                map.put(word, 1);
-            }
-        }
-
-        ArrayList<Map.Entry<String, Integer>> listForSort = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
-
-        Collections.sort(listForSort, new Comparator<Map.Entry<String,Integer>>() {
+        PriorityQueue<HeapNode> heap = new PriorityQueue<HeapNode>(k, new Comparator<HeapNode>() {
             @Override
-            public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2) {
-                if (o2.getValue() == o1.getValue()) {
-                    return o2.getKey().compareTo(o2.getKey());
+            public int compare(HeapNode o1, HeapNode o2) {
+                if (o2.freq == o1.freq) {
+                    return o1.word.compareTo(o2.word);
                 }else{
-                    return o2.getValue() - o1.getValue();
+                    return o2.freq - o1.freq;
                 }
             }
         });
 
+        ArrayList<HeapNode> nodes = new ArrayList<HeapNode>();
+
+        HashMap<String, HeapNode> map = new HashMap<String, HeapNode>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                HeapNode node = map.get(word);
+                node.freq++;
+                map.put(word, node);
+            }else{
+                // create new node
+                HeapNode node = new HeapNode();
+                node.word = word;
+                node.freq = 1;
+                node.index = nodes.size();
+
+                // put node in map and list
+                map.put(word,node);
+                nodes.add(node);
+            }
+        }
+
+        // get top k nodes by word freqs
+        for(HeapNode node : nodes) {
+            heap.offer(node);
+        }
+
         String rst[] = new String[k];
 
         for (int i=0; i<k; i++) {
-            rst[i] = (listForSort.get(i).getKey());
+            String word = heap.poll().word;
+            rst[i] = word;
         }
 
         return rst;
     }
+
+    class HeapNode{
+        String word;
+        int freq;
+        int index;
+    }
+
 }
