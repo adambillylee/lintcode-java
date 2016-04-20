42644700abf3555c01dc1a1536bac420594d2b5c
Adam Li <adam.li@ensighten.com>
2016-04-19 23:09:26 -0700

    tree iterators iterators

--- src/Binary_Tree_Postorder_Traversal_Iterative.java
+++ src/Binary_Tree_Postorder_Traversal_Iterative.java
@@ -7,23 +7,22 @@ import java.util.Stack;
 public class Binary_Tree_Postorder_Traversal_Iterative {
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> res = new ArrayList<Integer>();
-        if(root==null) {
+        if (root == null) {
             return res;
         }
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             TreeNode temp = stack.peek();
-            if(temp.left==null && temp.right==null) {
+            if (temp.left == null && temp.right == null) {
                 TreeNode pop = stack.pop();
                 res.add(pop.val);
-            }
-            else {
-                if(temp.right!=null) {
+            } else {
+                if (temp.right != null) {
                     stack.push(temp.right);
                     temp.right = null;
                 }
-                if(temp.left!=null) {
+                if (temp.left != null) {
                     stack.push(temp.left);
                     temp.left = null;
                 }
